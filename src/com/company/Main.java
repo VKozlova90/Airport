e7a205ec68f016378185e0b1e2c6c20d427ac3eb
VKozlova90 <fancygirl90@mail.ru>
2020-12-18 14:26:37 +0300

    fullInfo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,22 +16,32 @@ public class Main {
         PilotsFileReader reader = new PilotsFileReader(PILOTS_SOURCE_FILE);
         List <Pilots> pilots =  reader.readItens();
 
+        System.out.println("Старт записи в таблицу pilots");
         PilotsDbWriter writer = new PilotsDbWriter(pilots);
         writer.saveAll();
+        System.out.println("Запись в таблицу pilots завершена");
 
 
         AircraftFileReader reader1 = new AircraftFileReader(AIRCRAFT_SOURCE_FILE);
         List <Aircraft> aircraft =  reader1.readItens();
 
+        System.out.println("Старт записи в таблицу aircraft");
         AircraftDbWriter writer1 = new AircraftDbWriter(aircraft);
         writer1.saveAll();
+        System.out.println("Запись в таблицу aircraft завершена");
 
 
         FlightsFileReader reader2 = new FlightsFileReader(FLIGHTS_SOURCE_FILE);
         List <Flights> flights =  reader2.readItens();
 
+        System.out.println("Старт записи в таблицу flights");
         FlightsDbWriter writer2 = new FlightsDbWriter(flights);
         writer2.saveAll();
+        System.out.println("Запись в таблицу flights завершена");
+
+
+        FullInfoDbReader fullInfoDbReader= new FullInfoDbReader ();
+        fullInfoDbReader.readAll();
 
         }
 
---
+++ src/com/company/io/FullInfoDbReader.java
@@ -0,0 +1,45 @@
+package com.company.io;
+
+import com.company.util.DbConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FullInfoDbReader {
+
+    private  static final String SELECT_ALL = "SELECT f.id, f.flight_number, f.data, f.time FROM flights AS f INNER JOIN aircraft a ON a.id=f.aircraft INNER JOIN pilots p ON p.id=f.pilot";
+
+    public void readAll(){
+        Connection connection = DbConnectionUtil.detConnection();
+
+        try (PreparedStatement statement = connection .prepareStatement(SELECT_ALL)) {
+
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                int id = result.getInt("f.id");
+                String flight_number = result.getString("f.flight_number");
+                String data = result.getString("f.data");
+                String time = result.getString("f.time");
+                int tail_number = result.getInt("tail_number");
+                String brand = result.getString("brand");
+                String model = result.getString("model");
+                int passenger_capacity = result.getInt("passenger_capacity");
+                String name = result.getString("name");
+                String last_name = result.getString("last_name");
+                String pilot_code = result.getString("pilot_code");
+                String rank = result.getString("rank");
+
+                System.out.println(id + "-" + flight_number + "-" + data+ "-" +time+ "-" +tail_number+ "-" +brand
+                        + "-" + model+ "-" + passenger_capacity+ "-" +name+ "-" +last_name+ "-" +pilot_code
+                        + "-" + rank);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
